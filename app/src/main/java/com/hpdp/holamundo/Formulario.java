df9627d48b912e82a0edafb8ad6cf1e3124dfd57
P-Diaz <132587300+P-Diaz@users.noreply.github.com>
2023-11-13 15:51:34 -0500

    Validacion de Campo nombre

--- app/src/main/java/com/hpdp/holamundo/Formulario.java
+++ app/src/main/java/com/hpdp/holamundo/Formulario.java
@@ -1,8 +1,11 @@
 package com.hpdp.holamundo;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Formulario extends AppCompatActivity {
 
@@ -10,5 +13,23 @@ public class Formulario extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario);
+
+        EditText edNombres, edApellidos, edcedula, edCorreo, edDireccion;
+        Button btValidar;
+        btValidar = findViewById(R.id.btnValidar);
+        edNombres = findViewById(R.id.txtNombres);
+        edApellidos = findViewById(R.id.txtApellidos);
+        edcedula = findViewById(R.id.txtCedula);
+        edDireccion = findViewById(R.id.txtDireccion);
+        edCorreo = findViewById(R.id.txtCorreo);
+        btValidar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String Nombre = edNombres.getText().toString();
+                if (!Nombre.isEmpty()){
+                    Toast.makeText(Formulario.this, "Datos Ingresados Correctamente", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
